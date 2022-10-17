ea1a3efb08dd7965b45d465b755d08cbf51a4359
Aleks Zi <zi@zi.com>
2022-10-17 17:10:19 -0400

    fixed small vek bug

--- cpp/include/sysml/vek/vek_math.hpp
+++ cpp/include/sysml/vek/vek_math.hpp
@@ -12,7 +12,7 @@ namespace sysml
 
 template <arithmetic T, std::size_t N, arithmetic U>
 constexpr auto dot(vek<T, N> const& lhs, vek<U, N> const& rhs) noexcept
-    -> vek<decltype(std::declval<T>() * std::declval<U>()), N>
+    -> decltype(std::declval<T>() * std::declval<U>())
 {
     auto ret = lhs[0] * rhs[0];
     for (std::size_t i = 1; i < N; ++i)
